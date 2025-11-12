710db61c13a2e90a7617bf447a6cb22d564e1f0a
Your Name <you@example.com>
2025-11-12 11:18:38 +0900

    iv

--- bekseju/jscript/index.js
+++ bekseju/jscript/index.js
@@ -3,7 +3,7 @@ console.log(mainht);
 window.onscroll = function () {
   let ht = document.documentElement.scrollTop;
   console.log(ht);
-  if (ht > mainht) {
+  if (ht >= mainht) {
     document.querySelector("header").style.display = "none";
     document.querySelector(".topInner2").style.top = "0";
   } else {
