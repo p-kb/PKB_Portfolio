4f43eb9bfdcaaca6de7728fa4d2465b470b21918
Your Name <you@example.com>
2025-11-20 19:47:29 +0900

    a

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    sectionsColor: ["#0A0A0A", "#0A0A0A", "#0A0A0A", "#0A0A0A", "#B50000"],
+    anchors: ["firstPage", "secondPage", "3rdPage", "4thPage", "5thPage"],
+    menu: "#menu",
+    scrollOverflow: true,
+  });
+});
