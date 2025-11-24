9542f28119d474f803b3c58f46dc9ab931f60f25
Your Name <you@example.com>
2025-11-24 12:19:02 +0900

    cc

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#fullpage").fullpage({
-    sectionsColor: ["#0A0A0A", "#0A0A0A", "#0A0A0A", "#0A0A0A", "#B50000"],
-    anchors: ["firstPage", "secondPage", "3rdPage", "4thPage", "5thPage"],
-    menu: "#menu",
-    scrollOverflow: true,
-  });
-});
