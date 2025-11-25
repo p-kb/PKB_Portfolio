54ec6b45145afb4e0fa429e648934135c86d93a4
John Doe <johndoe@example.com>
2025-11-26 03:15:19 +0900

    a

---
+++ subway/js/index.js
@@ -0,0 +1,9 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
