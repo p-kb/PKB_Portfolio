650b0e8c4450441270d7a2005dace636a3eed81e
Your Name <you@example.com>
2025-11-27 14:52:34 +0900

    h

--- js/index.js
+++ js/index.js
@@ -12,3 +12,33 @@ $(".icon a").hover(
 );
 
 AOS.init();
+
+updateAOSOffset();
+
+function updateAOSOffset() {
+  const txtElements = document.querySelectorAll(".pwrap .txt");
+
+  if (!txtElements.length) return;
+
+  let offsetValue = 500;
+
+  // 화면이 줄면 offset = 200
+  if (window.innerWidth <= 1024) {
+    offsetValue = 300;
+  }
+
+  txtElements.forEach((el) => {
+    el.setAttribute("data-aos-offset", offsetValue);
+  });
+
+  // AOS 업데이트
+  if (typeof AOS !== "undefined") {
+    AOS.refreshHard();
+  }
+}
+
+// 페이지 로드 시
+window.addEventListener("load", updateAOSOffset);
+
+// 브라우저 사이즈 변경 시
+window.addEventListener("resize", updateAOSOffset);
